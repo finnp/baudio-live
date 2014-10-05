0fd384a2495cb9083d1c6a0fd042e88263bfce09
Finn <derfinn@gmail.com>
2014-10-06 01:02:20 +0200

    lol

--- baudio-live.js
+++ baudio-live.js
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
 var baudio = require('baudio')
-var file = process.argv[2]
-var path = require.resolve(file)
-var input = baudio(require(path))
-console.log('Playing', path, '...')
+var fs = require('fs')
+var path = require('path')
 
-setInterval(function () {
-  delete require.cache[path]
+var musicFile = process.argv[2]
+var musicFilePath = require.resolve(path.resolve(process.cwd(), musicFile))
+var input = baudio(require(musicFilePath))
+console.log('Playing', musicFilePath, '...')
+
+fs.watchFile(musicFilePath, { interval: 1 },function () {
+  console.log('Updated.')
+  delete require.cache[musicFilePath]
   try {
-    input._fn = require(path) // blocking :/
-  } catch(e) { /* could console.error here */ }
-}, 5)
+    input._fn = require(musicFilePath)
+  } catch(e) { console.error(e) }
+})
 
 input.play()
--- readme.md
+++ readme.md
@@ -5,6 +5,18 @@ npm install baudio-live -g
 ```
 
 ## Usage
+
+Given a file `music.js` like
+```js
+module.exports = function (t) {
+    return sin(t, 300) + sin(t,309)
+}
+```
+
+And the command:
+
 ```
 baudio-live music.js
 ```
+
+It will output the music and update it if you are changing that file.
