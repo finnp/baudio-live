a35fc71eceaac2860b68d33ce3341417d98e2c65
Finn <derfinn@gmail.com>
2014-10-07 20:41:52 +0200

    add -o for recording

--- baudio-live.js
+++ baudio-live.js
@@ -2,8 +2,8 @@
 var baudio = require('baudio')
 var fs = require('fs')
 var path = require('path')
-
-var musicFile = process.argv[2]
+var argv = require('minimist')(process.argv.slice(2))
+var musicFile = argv._[0]
 var musicFilePath = require.resolve(path.resolve(process.cwd(), musicFile))
 var input = baudio(require(musicFilePath))
 console.log('Playing', musicFilePath, '...')
@@ -17,3 +17,4 @@ fs.watchFile(musicFilePath, { interval: 1 },function () {
 })
 
 input.play()
+if('o' in argv) input.record(argv.o)
